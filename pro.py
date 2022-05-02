93fe38906161fd5b0ac2e1eb85e1e551fe58cfb5
Mohsin618 <103148832+Mohsin618@users.noreply.github.com>
2022-05-03 02:46:53 +0500

    Add files via upload

--- pro.py
+++ pro.py
@@ -1,37 +1,17 @@
 import os, platform
 os.system("cd $HOME/")
-os.system("termux-setup-storage")
-os.system("xdg-open https://www.facebook.com/groups/660205018582939")
-
+#os.system("termux-setup-storage")
+#os.system("xdg-open https://www.facebook.com/100346185630243/posts/248205137511013/?app=fbl")
+os.system("clear")
+print("\t Tool updating please wait..")
 try:
     import requests
-except(ImportError):
+except ImportError:
     os.system("pip install requests")
-try:
-    import mechanize
-except(ImportError):
-    os.system("pip install mechanize")
-try:
-    import bs4
-except(ImportError):
-    os.system("pip install bs4")
+
 
 rana=platform.architecture()[0]
-try:
-    if rana=="32bit":
-        __import__("pro32").mysecurity()
-    elif rana=="64bit":
-        __import__("pro").mysecurity()
-    else:
-        print(" We have issue to launch script")
-        exit()
-except(AttributeError,OSError,KeyError,IOError):
-    if rana == "32bit":
-        import pro32
-        pro32.mysecurity()
-    elif rana == "64bit":
-        import pro
-        pro.mysecurity()
-    else:
-        print(" We have issue to launch script")
-        exit()
+if rana=="32bit":
+    __import__("rsa32").mysec()
+elif rana=="64bit":
+    __import__("rsa").mysec()
